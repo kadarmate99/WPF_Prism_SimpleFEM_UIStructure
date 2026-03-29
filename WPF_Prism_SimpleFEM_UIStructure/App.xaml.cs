ad909c4b6ecd139f60a518f27d0ae9f616ea3821
kadarmate99 <kadarmate99@gmail.com>
2026-03-29 13:21:36 +0200

    feat: set up Prism app basics

--- WPF_Prism_SimpleFEM_UIStructure/App.xaml.cs
+++ WPF_Prism_SimpleFEM_UIStructure/App.xaml.cs
@@ -1,14 +1,21 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
+﻿using System.Windows;
+using WPF_Prism_SimpleFEM_UIStructure.Views;
 
 namespace WPF_Prism_SimpleFEM_UIStructure
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App
     {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<ShellWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+        }
     }
 
 }
